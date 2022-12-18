b7881204f30b6e0180141b2bca70b12f693ec69e
Syr010 <yana.syrtseva@yandex.ru>
2022-12-18 17:53:21 +0200

    new files

---
+++ pages_class/register.py
@@ -0,0 +1,26 @@
+import os
+
+from pages_class.base import WebPage
+from pages_class.elements import WebElement
+from pages_class.elements import ManyWebElements
+
+
+class MainPage(WebPage):
+
+    def __init__(self, web_driver, url=''):
+        url = 'https://b2c.passport.rt.ru'
+        super().__init__(web_driver, url)
+
+    registration_btn = WebElement(id='kc-register')
+
+    first_name_input = WebElement(name='firstName')
+
+    last_name_input = WebElement(name='lastName')
+
+    email_input = WebElement(id='address')
+
+    password_input = WebElement(id='password')
+
+    password_confirm_input = WebElement(id='password-confirm')
+
+    register_btn = WebElement(name='register')
---
+++ settings.py
@@ -0,0 +1,20 @@
+class TestData:
+
+
+    base_url = 'https://b2c.passport.rt.ru'
+
+    valid_email = 'test_testov013@mail.ru'
+    invalid_email = 'test_testov013@mail.ru$'
+    password001 = 'ForSkillFactory001'
+    password002 = 'ForSkillFactory002'
+    password003 = 'ForSkillFactory003'
+    short_password = 'ForSki'
+    lowercase_password = 'forskillfactory002'
+    cyrillic_password = 'ФорСкиллФактори002'
+    empty_password = ''
+    first_name_positive1 = 'Тест'
+    first_name_positive2 = '--'
+    last_name_positive1 = 'Тестов'
+    last_name_positive2 = '--'
+    valid_phone = '+79207284575'
+    invalid_phone = '+7r900$778008'
